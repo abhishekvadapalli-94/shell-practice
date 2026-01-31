28dc2ccd47b192d45541f8d906d8f5cb6e50368f
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-01 01:53:47 +0530

    shell practice commands

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo "Hello, World! I'm practicing shell scripting."
+
+echo "Current date and time: $(date)"
+echo "Current user: $(whoami)"
+echo "Current working directory: $(pwd)"
+echo "List of files in the current directory:"
+ls -l  
