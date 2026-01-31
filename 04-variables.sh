a7bdeb2c41a201cebc93d7a7fc122eb8a14740bf
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-01 02:36:31 +0530

    changed something

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,3 +2,14 @@
 
 Timestamp=$(date)
 echo "Script started at: $Timestamp"
+
+echo " special variables demo: "
+echo "Script name: $0"
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "Total number of arguments: $#"
+echo "All arguments as a single string: $*"
+echo "All arguments as separate strings: $@"
+echo "Process ID of the script: $$"
+echo "Exit status of the last command: $?" 
+echo "Last background process ID: $!"
