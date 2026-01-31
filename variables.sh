482d857dc58c4191bf42f54b32936e0744b177ce
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-01 02:27:20 +0530

    changed something

--- variables.sh
+++ variables.sh
@@ -2,3 +2,7 @@
 echo "Please enter your username:"
 read USERNAME
 echo "Your username is: $USERNAME"
+echo "Please enter your password:"
+read -s PASSWORD
+echo "Password received."
+echo "Welcome, $USERNAME!"
