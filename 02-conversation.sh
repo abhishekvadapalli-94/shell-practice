b5ce2c1ac70d370bc99b85110aa2f08b7c58d6c2
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-01 02:20:26 +0530

    changed something

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-echo "Suresh: Hello! What's your name?"
-echo "Ramesh: My name is Ramesh. Nice to meet you!"
-echo "Suresh: Nice to meet you too, Ramesh. How are you today?"
-echo "Ramesh: I'm doing well, thank you! How about you?"
-echo "Suresh: I'm great, thanks for asking. What do you like to do"
+Person1="Suresh"
+Person2="Ramesh"
 
+echo "$Person1: Hello! What's your name?"
+echo "$Person2: My name is Ramesh. Nice to meet you!"
+echo "$Person1: Nice to meet you too, Ramesh. How are you today?"
+echo "$Person2: I'm doing well, thank you! How about you?"
+echo "$Person1: I'm great, thanks for asking. What do you like to do?"
