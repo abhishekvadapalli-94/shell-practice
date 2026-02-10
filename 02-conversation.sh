44f993c92c311896af6ddb0e1ebfb466004a3dfd
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-10 00:10:39 -0500

    something

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -8,3 +8,5 @@ echo "$Person2: My name is $Person2. Nice to meet you!"
 echo "$Person1: Nice to meet you too, $Person2. How are you today?"
 echo "$Person2: I'm doing well, thank you! How about you?"
 echo "$Person1: I'm great, thanks for asking. What do you like to do?"
+echo "$Person2: I enjoy hiking and cooking. How about you?"
+echo "$Person1: I love playing sports and traveling. Maybe we can go hiking together sometime
