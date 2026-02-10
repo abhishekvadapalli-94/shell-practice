4fad214c77bd221c961666126e9db3b0ab6e969c
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-10 00:19:45 -0500

    something

--- datecommand.sh
+++ datecommand.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
-Timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+Timestamp=$(date +%s)
 echo "Current date and time: $Timestamp"
+
+sleep 10
+
+NewTimestamp=$(date +%s)
+echo "New date and time: $NewTimestamp"
+
+TimeDifference=$((NewTimestamp - Timestamp))
+echo "Time difference in seconds: $TimeDifference"
+
