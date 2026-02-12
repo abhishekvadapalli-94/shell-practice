451bc4dec418bc7ef146a91c04f6163349376380
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-11 20:19:36 -0500

    my msg

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,8 +3,11 @@
 Person1="$1"
 Person2="$2"
 
-echo "$Person1: Hello! What's your name?"
+echo "$Person1: Hello! What's your name? hello hello"
 echo "$Person2: My name is $Person2. Nice to meet you!"
 echo "$Person1: Nice to meet you too, $Person2. How are you today?"
 echo "$Person2: I'm doing well, thank you! How about you?"
 echo "$Person1: I'm great, thanks for asking. What do you like to do?"
+echo "$Person2: I enjoy coding and learning new technologies. How about you?"
+echo "$Person1: I also love coding! It's great to connect with someone who shares the same passion. Do you have any favorite programming languages?"
+echo "$Person2: I really like Python
