9ed7c13d1ec409bec39f0fea2e770fd4afed91c1
Abhishek <vadapalliabhi.94@gmail.com>
2026-02-11 20:27:33 -0500

    shell practice

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,21 +3,11 @@
 Person1="$1"
 Person2="$2"
 
-<<<<<<< HEAD
 echo "$Person1: Hello! What's your name? hello hello"
-=======
-echo "$Person1: Hello! What's your name hello?"
->>>>>>> 1b7865614ed3c490276241244d1e23f101df6444
 echo "$Person2: My name is $Person2. Nice to meet you!"
 echo "$Person1: Nice to meet you too, $Person2. How are you today?"
 echo "$Person2: I'm doing well, thank you! How about you?"
 echo "$Person1: I'm great, thanks for asking. What do you like to do?"
-<<<<<<< HEAD
 echo "$Person2: I enjoy coding and learning new technologies. How about you?"
 echo "$Person1: I also love coding! It's great to connect with someone who shares the same passion. Do you have any favorite programming languages?"
 echo "$Person2: I really like Python
-=======
-echo "$Person2: I enjoy hiking and cooking. How about you?"
-echo "$Person1: I love playing sports and traveling. Maybe we can go hiking together sometime!"
-echo "$Person2: That sounds like a great idea! I'd love to go hiking with you. Let's plan for it soon!"
->>>>>>> 1b7865614ed3c490276241244d1e23f101df6444
